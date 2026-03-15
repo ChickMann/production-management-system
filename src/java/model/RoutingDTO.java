10bb3603997550ed2686604d617fd5f736d919db
Trần Gia Bảo <trangiabao4461@gmail.com>
2026-03-15 19:22:50 +0700

    .

---
+++ src/java/model/RoutingDTO.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author se193234_TranGiaBao
+ */
+public class RoutingDTO {
+    private int routingId;
+    private String routingName;
+
+    public RoutingDTO() {
+    }
+
+    public RoutingDTO(int routingId, String routingName) {
+        this.routingId = routingId;
+        this.routingName = routingName;
+    }
+
+    public int getRoutingId() {
+        return routingId;
+    }
+
+    public void setRoutingId(int routingId) {
+        this.routingId = routingId;
+    }
+
+    public String getRoutingName() {
+        return routingName;
+    }
+
+    public void setRoutingName(String routingName) {
+        this.routingName = routingName;
+    }
+    
+    
+}
